1f694f23ede812c5633eae04cf94cf31002e7676
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-13 21:12:02 +0200

    Added Core of the API and .gitignore

---
+++ E-Commerce/API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return Ok(await _context.Products.FindAsync(id));
+        }
+    }
+}
---
+++ E-Commerce/API/Data/StoreContext.cs
@@ -0,0 +1,35 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Product one"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Product two"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Product three"
+                });
+        }
+    }
+}
