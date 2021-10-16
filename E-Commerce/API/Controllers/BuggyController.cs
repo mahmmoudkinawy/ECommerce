2fa7d933d8f40ad08df5b90882083a2902e4e3a1
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-16 13:44:24 +0200

    Added buggy controller

---
+++ E-Commerce/API/Controllers/BuggyController.cs
@@ -0,0 +1,47 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            var thing = _context.Products.Find(-1);
+
+            if (thing == null) return NotFound();
+
+            return Ok();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(-1);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok(thingToReturn);
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("bad-request/{id}")]
+        public ActionResult GetNotFound(int id)
+        {
+            return Ok();
+        }
+    }
+}
--- E-Commerce/API/Controllers/ProductsController.cs
+++ E-Commerce/API/Controllers/ProductsController.cs
@@ -5,7 +5,6 @@ using Core.Interfaces;
 using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Controllers
