f2b85d1beced38c2543c2b753afcf1c8de8506fe
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-16 15:17:06 +0200

    Make end points very clear in the API and Swagger

--- E-Commerce/API/Controllers/ProductsController.cs
+++ E-Commerce/API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 ﻿using API.DTOs;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -37,10 +39,14 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
 
             return Ok(_mapper.Map<ProductToReturnDto>(product));
         }
