4fdd02bfba3cb8fdcd9e0f2c71482c4d47abda1c
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-26 15:17:00 +0200

    Make pagination a shared component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
