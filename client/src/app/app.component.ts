3dee00cd824eb78ede87ecea54c7571aa9af0b47
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-26 10:39:24 +0200

    Added service and make it work

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
 
@@ -12,17 +12,7 @@ export class AppComponent implements OnInit {
   title = 'Skinet';
   products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-        this.products = response.data;
-        console.log(response);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  ngOnInit(): void {}
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShopService {
-  constructor() {}
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IPagination> {
+    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
 }
